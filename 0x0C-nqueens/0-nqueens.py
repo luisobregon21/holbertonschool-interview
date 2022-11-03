24e13893d028a9db0f6b6e5bc4212e42ac90185e
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-11-02 21:22:35 -0400

    add new project

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -8,13 +8,47 @@ Solution for the N queens problem.
 
 import sys
 
+
+def place_queen(queen, column, prev_solver):
+    placement_queen = []
+    for array in prev_solver:
+        for idx in range(column):
+            if is_safe(queen, idx, array):
+                placement_queen.append(array + [idx])
+    return placement_queen
+
+
+def is_safe(queen, idx, array):
+    if idx in array:
+        return (False)
+    else:
+        return all(abs(array[column] - idx) != queen - column
+                   for column in range(queen))
+
+
 if __name__ == '__main__':
-    # Usage: nqueens N
-    # If the user called the program with the wrong number of arguments, print Usage: nqueens N, followed by a new line, and exit with the status 1
-    # where N must be an integer greater or equal to 4
-    # If N is not an integer, print N must be a number, followed by a new line, and exit with the status 1
-    # If N is smaller than 4, print N must be at least 4, followed by a new line, and exit with the status 1
-    # The program should print every possible solution to the problem
-    # One solution per line
-    # Format: see example
-    # You don’t have to print the solutions in a specific order
+    args = sys.argv
+
+    if len(args) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(args[1])
+    except Exception as e:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    placement = [[]]
+    for queen in range(N):
+        placement = place_queen(queen, N, placement)
+
+    for array in placement:
+        ans = []
+        for q, x in enumerate(array):
+            ans.append([q, x])
+        print(ans)
