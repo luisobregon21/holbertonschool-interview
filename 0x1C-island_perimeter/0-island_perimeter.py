8a177aca76caff26dcff6f57b4d36bc36e93dcd0
Luis Obregon <luis@coquihealth.com>
2023-03-16 12:05:41 -0400

    fix pycodestyle comment length

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -22,7 +22,7 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 perimeter += 4
                 # check the adjacent cells and subtract
-                #from the perimeter if they are also land
+                # from the perimeter if they are also land
                 if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j-1] == 1:
